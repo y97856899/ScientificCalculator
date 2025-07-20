710d87b29f2f6fb64d50d0bc35e1d2d20182b37b
yousuf.shaaili <alshaili37@gmail.com>
2025-07-20 16:29:36 +0400

    feat: Add subtraction functionality (subtract, performSubtraction)

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -7,5 +7,24 @@ public class ScientificCalculator {
     public static double add(double first,double second){
         return first+second;
     }
-    
+    public static double subtract(double first,double second){
+        return first-second;
+    }
+
+
+
+
+
+
+
+    public static void performOperation(Scanner scanner){
+        switch (choice) {
+                    case 1:
+                        result = add(num1, num2);
+                        break;
+                    case 2:
+                        result = subtract(num1, num2);
+                        break;
+        }
+    }                        
 }
