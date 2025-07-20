8ce3d9a2d613be96ccddbe4b5bb8bc4b6154ff32
yousuf.shaaili <alshaili37@gmail.com>
2025-07-20 16:27:01 +0400

    feat: Implement addition functionality (add)

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -4,4 +4,8 @@ public class ScientificCalculator {
     }
     public static void displayMenu(){
     }
+    public static double add(double first,double second){
+        return first+second;
+    }
+    
 }
